318da0fc6372f50b7bcebb73fd60a652e8c866e8
Thijs Haker <theage2001@gmail.com>
2022-10-27 10:10:24 +0200

    Moved stuff out of library package

--- clu/lib/const.go
+++ clu/lib/const.go
@@ -1,6 +1,10 @@
 package lib
 
 const (
+	ExitDef       = 0
+	ExitErr       = 1
+	UrlFormat     = "http://%s:1064%s"
+	JsonMime      = "application/json"
 	PortSignature = ":1064"
 	RouteAdd      = "/add"
 	RouteDelete   = "/del"
