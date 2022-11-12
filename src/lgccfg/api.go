1fe90a0de83ea5a4585588b15becb6660856833f
Thijs Haker <theage2001@gmail.com>
2022-11-12 15:12:53 +0100

    Filled in api.go

--- src/lgccfg/api.go
+++ src/lgccfg/api.go
@@ -1,19 +1,48 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func apiCreate(w http.ResponseWriter, r *http.Request) {
+	var (
+		key string = r.URL.Query().Get("key")
+		val string = r.URL.Query().Get("val")
+	)
 
+	kvs.Create(key, val)
 }
 
 func apiRead(w http.ResponseWriter, r *http.Request) {
+	var (
+		key string = r.URL.Query().Get("key")
+		val string
+		ok  bool
+	)
 
+	if val, ok = kvs.Read(key); !ok {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", val)
 }
 
 func apiUpdate(w http.ResponseWriter, r *http.Request) {
+	var (
+		key string = r.URL.Query().Get("key")
+		val string = r.URL.Query().Get("val")
+	)
 
+	kvs.Update(key, val)
 }
 
 func apiDelete(w http.ResponseWriter, r *http.Request) {
+	var (
+		key string = r.URL.Query().Get("key")
+		val string = r.URL.Query().Get("val")
+	)
 
+	kvs.Delete(key, val)
 }
